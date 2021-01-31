98e8677a5503360dc2b3af29b2806a767d55fe7b
Halyna Urbanska <webenot@mail.ua>
2021-01-31 11:02:44 +0200

    fix: Fix resolve decorator

--- src/annotations/resolve.annotation.ts
+++ src/annotations/resolve.annotation.ts
@@ -1,20 +1,20 @@
 import { CONTAINER_CONTEXT, Container } from '../container';
 
-export function resolve(target: any, propertyName: string) {
+export function resolve (target: any, propertyName: string) {
 
   const original = Reflect.getMetadata('design:type', target.constructor.prototype, propertyName);
 
   const $resolve = Reflect.getMetadata('resolve', target.constructor) || [];
-  $resolve.push([propertyName, original]);
+  $resolve.push([ propertyName, original ]);
 
   Reflect.defineMetadata('resolve', $resolve, target.constructor);
 
   Object.defineProperty(target.constructor.prototype, propertyName, {
-    get() {
+    get () {
       if (this.context && this.context instanceof Container) {
         return this.context.get(original);
       }
-      return this[CONTAINER_CONTEXT].get(original)
-    }
+      return this[CONTAINER_CONTEXT].get(original);
+    },
   });
 }
